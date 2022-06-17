59d8bf40c65d0d096cdf4d2d0dbf9d5addc700cd
Fabien Udriot <fabien@omic.ch>
2022-06-17 16:45:00 +0200

    Release 3.0.0 - <https://github.com/visol/ext-translationapi/compare/2.0.1...3.0.0>

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## 3.0.0 (2022-06-17)
+
+[TASK] Remove routing and adjust docs accordingly
+[TASK] Refactor php annotation and copyright header
+[TASK] v11 compatibility
+[TASK] Rename composer name
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,6 @@
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Translation API',
     'description' => 'API to fetch translation units.',
-    'version' => '3.0.0',
     'state' => 'stable',
-    'author_company' => 'Visol',
+    'author_company' => 'visol - digitale Dienstleistungen GmbH',
 );
